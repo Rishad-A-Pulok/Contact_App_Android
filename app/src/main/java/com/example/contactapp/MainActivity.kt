32b79978e1fb154518f53bb5e78b876f98ab4729
Rishad Amin Pulok <rishad15-2117@diu.edu.bd>
2022-04-09 16:51:17 +0600

    design updated

--- app/src/main/java/com/example/contactapp/MainActivity.kt
+++ app/src/main/java/com/example/contactapp/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.contactapp
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar!!.setBackgroundDrawable(ColorDrawable(Color.parseColor("#5EA5DD")))
     }
 }
